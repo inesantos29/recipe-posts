ef5e35daf65b3bd2b26bc81aff12761a0cad03d0
inesantos29 <ines.arts@hotmail.com>
2019-08-19 16:21:51 +0100

    Create SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+import { SearchBar } from './components/SearchBar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <>
+        <header>
+            <h1 className="title">Recipe Posts</h1>
+            <hr className="hr-header" />
+        </header>
+
+        <SearchBar/>
+
+        <Row>
+            <Col xs={12} md={6}>
+            </Col>
+            <Col xs={12} md={6}>
+            </Col>
+        </Row>
+    </>
   );
 }
 
